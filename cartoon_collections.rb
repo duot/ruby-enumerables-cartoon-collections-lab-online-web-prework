6330d82fbb9d0752a5cef6d2ad5f4dd7bf7ec0b4
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-20 15:30:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,3 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
-
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def roll_call_dwarves dwarves
+  dwarves.each_with_index { |e, idx| puts "#{idx}. #{e}"}
 end
