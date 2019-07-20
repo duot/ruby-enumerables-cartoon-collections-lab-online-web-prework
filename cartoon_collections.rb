6e7ecfece80873d16c77478aefa392dc14e6aaaa
optimistic-writer-7178 <duot.jim@gmail.com>
2019-07-20 15:35:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,6 @@
-def roll_call_dwarves d
-  d.each_with_index { |e, idx | puts "#{idx +1}. #{e}"}
+def roll_call_dwarves dwarves
+  dwarves.each_with_index { |e, idx | puts "#{idx +1}. #{e}"}
 end
+
+
+def summon_captain_planet planeteer_calls
